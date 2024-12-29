80e01696262a08393783ac8b80818f963363e511
n0bisuke <rsksound@gmail.com>
2024-12-29 23:36:42 +0900

    update

--- nodes/config/config.js
+++ nodes/config/config.js
@@ -1,18 +1,43 @@
 module.exports = function(RED) {
+    const { Client } = require('@line/bot-sdk');
+
     function main(n) {
         RED.nodes.createNode(this, n);
-        this.name = n.name;
-        this.LineChannelSecret = this.credentials.LineChannelSecret;
-        this.LineChannelAccessToken = this.credentials.LineChannelAccessToken;
+        // コンフィグノードの初期化
     }
- 
+
     RED.nodes.registerType("lineConfig", main, {
         defaults: {
-            name: {value: ""}
+            botName: { type: "text" }
         },
         credentials: {
-            LineChannelSecret: {type: "text"},
-            LineChannelAccessToken: {type: "text"}
+            LineChannelSecret: { type: "text" },
+            LineChannelAccessToken: { type: "text" }
         }
     });
- }
+
+    // Admin HTTP API 用のエンドポイント
+    RED.httpAdmin.post('/line-config/bot-info', async function(req, res) {
+        try {
+            const channelSecret = req.body.channelSecret;
+            const channelAccessToken = req.body.channelAccessToken;
+            if (!channelSecret || !channelAccessToken) {
+                return res.status(400).send("No channelSecret or channelAccessToken");
+            }
+
+            const clientConfig = {
+                channelSecret,
+                channelAccessToken
+            };
+            const client = new Client(clientConfig);
+
+            const botInfo = await client.getBotInfo();
+            // botInfo.displayName, botInfo.pictureUrl などを含む
+            return res.json(botInfo);
+
+        } catch (err) {
+            console.error("Failed to getBotInfo:", err);
+            res.status(500).send(err.message || "Failed to get botInfo");
+        }
+    });
+}
---
+++ nodes/getBotInfo/getBotInfo.js
@@ -0,0 +1,46 @@
+module.exports = (RED) => {
+    'use strict';
+    
+    const line = require('@line/bot-sdk');
+
+    const main = function(config){
+        const node = this;
+        RED.nodes.createNode(node, config);
+
+        let lineconfig = {};
+        try {
+            lineconfig = require('../../env');
+        } catch (error) {
+            node.lineConfig = RED.nodes.getNode(config.lineConfig); //共通のlineConfigを取得
+
+            lineconfig = { 
+                channelSecret: node.lineConfig.credentials.LineChannelSecret,
+                channelAccessToken: node.lineConfig.credentials.LineChannelAccessToken
+            };
+        }
+        if(lineconfig.channelAccessToken === undefined || lineconfig.channelAccessToken === '') {
+            console.error('LINE token not found');
+            return;
+        }
+        const client = new line.messagingApi.MessagingApiClient(lineconfig);
+
+        // ここでLINE APIを呼び出す処理
+        node.on('input', async(msg, send, done) =>  {
+
+            try {
+                const res = await client.getBotInfo();
+                msg.payload = res;
+                send(msg);
+            } catch (error) {
+                console.error(error.body);
+                node.error(error.body, msg.payload);
+                send(msg);
+                done(error);
+            }
+
+            console.log("Message received:", msg.payload);
+        });
+    }
+
+    RED.nodes.registerType("getBotInfo", main);
+}
